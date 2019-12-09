0ea6f3010a455fbcef436e6860ba12113c37e9ed
samperay <sunlnx@gmail.com>
2019-12-09 15:52:44 +0530

    modified files for project specific

--- projects/amzims/inventorymgmt/views.py
+++ projects/amzims/inventorymgmt/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from inventorymgmt.models import Inventory
 
 def home(request):
-    return HttpResponse('<p> Home View </p>')
+    getdetails = Inventory.objects.all()
+    #output = ",".join([str(home) for home in getdetails])"
+    return HttpResponse(getdetails)
 
-def inventory_detail(requestm, instanceid):
+def inventory_detail(request, instanceid):
     return HttpResponse('<p>Instance details with id {} </p>'.format(instanceid))
