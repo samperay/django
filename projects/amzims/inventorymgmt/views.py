40696b25ce75c34968a63f717352d7ed05cb71b9
samperay <sunlnx@gmail.com>
2020-01-06 18:58:04 +0530

    inventory api added

--- projects/amzims/inventorymgmt/views.py
+++ projects/amzims/inventorymgmt/views.py
@@ -5,3 +5,6 @@ from inventorymgmt.models import Inventory
 def default(request):
     # Displaying default message for project
     return HttpResponse("Welcome to Amazon Inventory Management Systems !")
+
+def inventory(request):
+    return HttpResponse("<html> <h1> Amazon Inventory List </h1> </html> ")
